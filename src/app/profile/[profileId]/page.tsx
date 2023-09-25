ddcff9997eca1e2eb5a21bc0f55c0137e82705e7
ArtemFedorchuk <fedorchuk.zver@gmail.com>
2023-09-25 23:22:11 +0300

    Update lint

--- src/app/profile/[profileId]/page.tsx
+++ src/app/profile/[profileId]/page.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import Link from "next/link";
+import Link from 'next/link';
 
 const ProfileDetail = ({ params }: { params: { profileId: string } }) => {
-    return (
-        <div>
-            <h1>Profile Details: {params.profileId}</h1>
-            <Link href={{
-                pathname: '/profile',
-            }} className="bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded">
-                Profile
-            </Link>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Profile Details: {params.profileId}</h1>
+      <Link
+        href={{
+          pathname: '/profile',
+        }}
+        className="bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded"
+      >
+        Profile
+      </Link>
+    </div>
+  );
 };
 
 export default ProfileDetail;
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import Link from "next/link";
+
+import Link from 'next/link';
 
 const Profile = () => {
-    return (
-        <div>
-            <h1>Profile</h1>
-            <Link href={{
-                pathname: '/profile/settings'
-            }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Settings
-            </Link>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Profile</h1>
+      <Link
+        href={{
+          pathname: '/profile/settings',
+        }}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Settings
+      </Link>
+    </div>
+  );
 };
 
 export default Profile;
